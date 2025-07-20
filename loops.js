31eccfde4d531cbb5c6a59ac0806f068f8970b66
dineshvg10 <dinesh1397@gmail.com>
2025-07-20 21:34:30 +0530

    loos

---
+++ loops.js
@@ -0,0 +1,26 @@
+// for loop
+for(let a=1;a<=5;a++){
+console.log(a);
+}
+
+for (let b=10;b>=1;b--){
+console.log(b);
+}
+
+//while loop
+let ab =10
+while(ab>=1){
+    console.log(ab);
+    ab--
+}
+
+// do while loop
+let abd =1
+do{
+    abd++
+    console.log(abd);
+    //abd++
+}while(abd>=5)
+
+    // added for git push
+    
